50dc44b00f0904d443fcb44077c5a379156002e7
Paulo Renato <paulorenatorf@hotmail.com>
2023-06-25 02:27:30 -0300

    builde

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,9 +5,10 @@ import helmet from "helmet";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
-  app.enableCors({
-    origin: "http://localhost:3000",
-  });
+  // app.enableCors({
+  //   origin: "http://localhost:3000",
+  // });
+  app.enableCors();
   await app.listen(5000);
 }
 bootstrap();
